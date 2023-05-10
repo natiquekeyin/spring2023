917acde4fc4e2a86848c96ceae8ed051a17f357b
Noman Atique <noman.atique@keyin.com>
2023-05-10 12:28:17 -0400

    May10

--- JavaScript/Week2/scripts/script.js
+++ JavaScript/Week2/scripts/script.js
@@ -1,5 +1,15 @@
-var myTitle = document.title;
+// var myTitle = document.title;
 
-// console.log("The title of the document is " + myTitle);
+// // console.log("The title of the document is " + myTitle);
 
-console.log(`The title of the document is ${myTitle}`);
+// console.log(`The title of the document is ${myTitle}`);
+
+// var x = document.getElementById("p2");
+
+// console.log(`The element with id p2 is ${x}`);
+
+var div = document.createElement("div");
+var text = document.createTextNode("Hi I am a div");
+
+div.appendChild(text);
+document.body.appendChild(div);
