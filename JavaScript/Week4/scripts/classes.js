4526386c40e2fa1f6b5921df40a98f4871cf44fc
Noman Atique <noman.atique@keyin.com>
2023-05-29 08:30:31 -0400

    May29

--- JavaScript/Week4/scripts/classes.js
+++ JavaScript/Week4/scripts/classes.js
@@ -41,6 +41,10 @@ class Book {
       this.showAlert("Wrong area clicked! Click on X", "error");
     }
   }
+
+  // static abc() {
+  //   console.log("I am static function inside class..");
+  // }
 }
 
 // Get the reference to the form...
@@ -59,6 +63,7 @@ form.addEventListener("submit", function (evt) {
   } else {
     book.addBookToList(book);
     book.showAlert("Book Successfully added!", "success");
+    Store.addBook(book);
     book.clearFields();
   }
 
@@ -67,8 +72,52 @@ form.addEventListener("submit", function (evt) {
 
 document.querySelector("#book-list").addEventListener("click", function (evt) {
   var book = new Book();
+  //Book.abc(); //NOT object level .. a class level .. DIRECTLY with the class name...
   book.deleteBook(evt.target);
   evt.preventDefault();
 });
 
+// May 29,2023
+
 // local Storage.. to store books permanently...
+// browser's memory..we use to store objects in it and retrieve them....
+// This Store class will i)Storing books in local storage...ii)Retrieving the books from LS. iii)Deleting the books for LS.
+class Store {
+  // getBooks will retrieve the books from LS...
+  static getBooks() {
+    var books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+      // stringify()
+    }
+
+    return books;
+  }
+
+  static addBook(book) {
+    // bring the already stored books from memory and save this array in books variable...
+    var books = Store.getBooks();
+    // Push the passed book to this funtion into the already available books...
+    books.push(book);
+
+    // update the local Storage...add this created books array to the memory...
+
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+
+  static displayBooks() {
+    var books = Store.getBooks();
+
+    books.forEach((book) => {
+      var objBook = new Book();
+
+      objBook.addBookToList(book);
+    });
+  }
+
+  static removeBook() {}
+}
+
+document.addEventListener("DOMContentLoaded", Store.displayBooks);
