099f39a724eed40dd3590bd9fd9ee0450057762e
Noman Atique <noman.atique@keyin.com>
2023-06-06 11:25:33 -0400

    June6

--- JavaScript/Week6/firstapp/src/App.js
+++ JavaScript/Week6/firstapp/src/App.js
@@ -1,25 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Noman Atique -first application in React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="demo">
+        <h1>My First Application -</h1>
+        <p>React is fun to learn!!!!</p>
+      </div>
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    </>
   );
+  // the above all is "jsx.."
 }
 
 export default App;
