ced7b669a33fd8948899375aa5cfea41fc93bfe9
Noman Atique <noman.atique@keyin.com>
2023-07-24 08:30:40 -0400

    July24

---
+++ JavaScript/Week11/thirdapp/src/__tests__/sometest.spec.jsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import Welcome from "../components/welcome/Welcome";
+
+test("Welcome renders correctly", () => {
+  render(<Welcome />);
+  const textElement = screen.getByText("Hello");
+  expect(textElement).toBeInTheDocument();
+});
--- JavaScript/Week11/thirdapp/src/components/addperiod/AddPeriod.test.jsx
+++ JavaScript/Week11/thirdapp/src/components/addperiod/AddPeriod.test.jsx
@@ -2,13 +2,19 @@ import { render, screen } from "@testing-library/react";
 import AddPeriod from "./AddPeriod";
 
 describe("Period", () => {
-  test("Add Period renders correctly", () => {
+  it("Add Period renders correctly", () => {
     render(<AddPeriod />);
     const textElement = screen.getByText("Period");
     expect(textElement).toBeInTheDocument();
   });
 
-  test("Add period with parameter and period", () => {
+  fit("Add period with parameter and period", () => {
+    render(<AddPeriod name="Alex" />);
+    const textElement = screen.getByText("Period Alex.");
+    expect(textElement).toBeInTheDocument();
+  });
+
+  it("Add period with parameter and period", () => {
     render(<AddPeriod name="Alex" />);
     const textElement = screen.getByText("Period Alex.");
     expect(textElement).toBeInTheDocument();
