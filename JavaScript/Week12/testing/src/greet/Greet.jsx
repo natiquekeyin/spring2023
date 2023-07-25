2bbe48168aaa64d4799f3ae225d72d635edc5a89
Noman Atique <noman.atique@keyin.com>
2023-07-25 11:24:49 -0400

    july25

---
+++ JavaScript/Week12/testing/src/greet/Greet.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Greet = () => {
+  return (
+    <>
+      <h1>The greet form</h1>
+      <h2>Enter your name</h2>
+      <form>
+        <div>
+          <label htmlFor="comment">Comment</label>
+          <textarea name="comment" id="comment" cols="30" rows="10"></textarea>
+        </div>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            placeholder="Enter your name here"
+            value="keyin"
+            onChange={() => {}}
+          />
+        </div>
+        <div>
+          <label htmlFor="location">Location</label>
+          <select id="location">
+            <option value="">Select a location</option>
+            <option value="on">Ontario</option>
+            <option value="nl">Newfoundland</option>
+            <option value="al">Alberta</option>
+            <option value="pe">Prince Edward Island</option>
+          </select>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" id="terms" />I agree
+          </label>
+        </div>
+        <button>Submit</button>
+      </form>
+    </>
+  );
+};
+
+export default Greet;
---
+++ JavaScript/Week12/testing/src/greet/greet.test.jsx
@@ -0,0 +1,43 @@
+import { render, screen } from "@testing-library/react";
+import Greet from "./Greet";
+
+describe("Greet Form", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const nameElement = screen.getByRole("textbox", {
+      name: "Name",
+    });
+    expect(nameElement).toBeInTheDocument();
+
+    const comboElement = screen.getByRole("combobox");
+    expect(comboElement).toBeInTheDocument();
+
+    const checkBoxElement = screen.getByRole("checkbox");
+    expect(checkBoxElement).toBeInTheDocument();
+
+    const nameElement2 = screen.getByRole("textbox", {
+      name: "Comment",
+    });
+    expect(nameElement2).toBeInTheDocument();
+
+    const pageHeading = screen.getByRole("heading", { level: 1 });
+    expect(pageHeading).toBeInTheDocument();
+
+    const pageHeading2 = screen.getByRole("heading", {
+      level: 2,
+    });
+    expect(pageHeading2).toBeInTheDocument();
+
+    const nameElement3 = screen.getByLabelText("Name");
+    expect(nameElement3).toBeInTheDocument();
+
+    const lblElement = screen.getByLabelText("I agree");
+    expect(lblElement).toBeInTheDocument();
+
+    const plcElement = screen.getByPlaceholderText("Enter your name here");
+    expect(plcElement).toBeInTheDocument();
+
+    const nameElement4 = screen.getByDisplayValue("keyin");
+    expect(nameElement4).toBeInTheDocument();
+  });
+});
