34be7f07efd59717f7027f676e7ba3269238db21
Noman Atique <noman.atique@keyin.com>
2023-07-31 11:42:35 -0400

    July31_1

--- JavaScript/Week12/testing/src/myevents/event.test.jsx
+++ JavaScript/Week12/testing/src/myevents/event.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Event from "./Event";
+import user from "@testing-library/user-event";
 
 describe("Event", () => {
   test("renders correctly", () => {
@@ -16,4 +17,16 @@ describe("Event", () => {
     const countElement = screen.getByRole("heading");
     expect(countElement).toHaveTextContent(/^0$/);
   });
+
+  test("renders a count of 1 after clicking the increment button", async () => {
+    user.setup();
+    render(<Event />);
+    const incrementButton = screen.getByRole("button", { name: "Increment" });
+    await user.click(incrementButton);
+
+    const countElement = screen.getByRole("heading");
+    expect(countElement).toHaveTextContent(/^1$/);
+  });
+
+  test("renders a count of 2 after clicking the icrment button twice", () => {});
 });
